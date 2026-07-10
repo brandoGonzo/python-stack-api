ef2c381560a6c6eddd679bd39aff72aee9fd8950
Brandon Gonzales <164552956+brandoGonzo@users.noreply.github.com>
2026-07-10 05:41:14 -0400

    Update test_crud.py
    Added pytest and initial test functions.

--- test_crud.py
+++ test_crud.py
@@ -6,7 +6,8 @@ import crud
 from database import SessionLocal
 
 # use a test date of 4/1/2024 to test the min_last_changed_date.
-test_date = date(2024,4,1)
+test_date = date(2024, 4, 1)
+
 
 @pytest.fixture(scope="function")
 def db_session():
@@ -15,20 +16,24 @@ def db_session():
     yield session
     session.close()
 
+
 def test_get_player(db_session):
     """Tests you can get the first player"""
-    player = crud.get_player(db_session, player_id = 1001)
+    player = crud.get_player(db_session, player_id=1001)
     assert player.player_id == 1001
+
 
 def test_get_players(db_session):
     """Tests that the count of players in the database is what is expected"""
     players = crud.get_players(db_session, skip=0, limit=10000,
-                                min_last_changed_date=test_date)
+                               min_last_changed_date=test_date)
     assert len(players) == 1018
+
 
 def test_get_players_by_name(db_session):
     """Tests that the count of players in the database is what is expected"""
-    players = crud.get_players(db_session, first_name="Bryce", last_name="Young")
+    players = crud.get_players(
+        db_session, first_name="Bryce", last_name="Young")
     assert len(players) == 1
     assert players[0].player_id == 2009
 
@@ -39,14 +44,16 @@ def test_get_all_performances(db_session):
     performances = crud.get_performances(db_session, skip=0, limit=18000)
     assert len(performances) == 17306
 
+
 def test_get_new_performances(db_session):
     """Tests that the count of performances in the database is 
     what is expected"""
-    performances = crud.get_performances(db_session, skip=0, limit=10000, 
+    performances = crud.get_performances(db_session, skip=0, limit=10000,
                                          min_last_changed_date=test_date)
     assert len(performances) == 2711
 
-#test the count functions
+
+# test the count functions
 def test_get_player_count(db_session):
     player_count = crud.get_player_count(db_session)
     assert player_count == 1018
