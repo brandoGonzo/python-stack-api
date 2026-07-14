6681dfca04ad0327dd20e1f8c8cd360d96b79fde
Brandon Gonzales <164552956+brandoGonzo@users.noreply.github.com>
2026-07-14 05:35:43 -0400

    Update schemas.py
    Added PlayerBase and Player Pydantic schemas to schemas.py.

--- schemas.py
+++ schemas.py
@@ -3,10 +3,26 @@ from pydantic import BaseModel, ConfigDict
 from typing import List
 from datetime import date
 
+
 class Performance(BaseModel):
-    model_config = ConfigDict(from_attributes = True)
-    performance_id : int
-    player_id : int
-    week_number : str
-    fantasy_points : float
-    last_changed_date : date
+    model_config = ConfigDict(from_attributes=True)
+    performance_id: int
+    player_id: int
+    week_number: str
+    fantasy_points: float
+    last_changed_date: date
+
+
+class PlayerBase(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+    player_id: int
+    gsis_id: str
+    first_name: str
+    last_name: str
+    position: str
+    last_changed_date: date
+
+
+class Player(PlayerBase):
+    model_config = ConfigDict(from_attributes=True)
+    performances: List[Performance] = []
