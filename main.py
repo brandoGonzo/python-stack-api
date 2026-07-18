c589607d54c22da2d3d135423365283e9983a658
Brandon Gonzales <164552956+brandoGonzo@users.noreply.github.com>
2026-07-18 06:31:24 -0400

    Update main.py
    Added initial set of endpoints for API.

--- main.py
+++ main.py
@@ -42,3 +42,72 @@ def read_players(skip: int = 0, limit: int = 100,
                                first_name=first_name,
                                last_name=last_name)
     return players
+
+
+@app.get("/v0/players/{player_id}", response_model=schemas.Player)
+def read_player(player_id: int, db: Session = Depends(get_db)):
+    player = crud.get_player(db, player_id=player_id)
+
+    # If the player is not found, raise a 404 error
+    if player is None:
+        raise HTTPException(status_code=404, detail="Player not found")
+
+    return player
+
+
+@app.get("/v0/performances/", response_model=list[schemas.Performance])
+def read_performances(skip: int = 0,
+                      limit: int = 100,
+                      minimum_last_changed_date: date = None,
+                      db: Session = Depends(get_db)):
+
+    performances = crud.get_performances(db, skip=skip, limit=limit,
+                                         min_last_changed_date=minimum_last_changed_date)
+
+    return performances
+
+
+@app.get("/v0/leagues/{league_id}", response_model=schemas.League)
+def read_league(league_id: int, db: Session = Depends(get_db)):
+    league = crud.get_league(db, league_id=league_id)
+
+    # If the league is not found, raise a 404 error
+    if league is None:
+        raise HTTPException(status_code=404, detail="League not found")
+
+    return league
+
+
+@app.get("/v0/leagues/", response_model=list[schemas.League])
+def read_leagues(skip: int = 0, limit: int = 100, minimum_last_changed_date: date = None,
+                 league_name: str = None,
+                 db: Session = Depends(get_db)):
+
+    leagues = crud.get_leagues(db, skip=skip, limit=limit,
+                               min_last_changed_date=minimum_last_changed_date,
+                               league_name=league_name)
+
+    return leagues
+
+
+@app.get("/v0/teams/", response_model=list[schemas.Team])
+def read_teams(skip: int = 0, limit: int = 100, minimum_last_changed_date: date = None,
+               team_name: str = None,
+               league_id: int = None,
+               db: Session = Depends(get_db)):
+
+    teams = crud.get_teams(db, skip=skip, limit=limit,
+                           min_last_changed_date=minimum_last_changed_date,
+                           team_name=team_name,
+                           league_id=league_id)
+
+    return teams
+
+
+@app.get("/v0/counts/", response_model=schemas.Counts)
+def get_count(db: Session = Depends(get_db)):
+    # Get the counts of the leagues, teams, and players
+    counts = schemas.Counts(league_count=crud.get_league_count(db), team_count=crud.get_team_count(db),
+                            player_count=crud.get_player_count(db))
+
+    return counts
